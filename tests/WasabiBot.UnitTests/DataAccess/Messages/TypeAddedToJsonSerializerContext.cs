bfef73afc449968de75c1ec57580158b79d668ed
Kyle Hickman <kyle@txhickman.com>
2024-10-11 18:19:21 -0500

    reorganize solution

---
+++ tests/WasabiBot.UnitTests/DataAccess/Messages/TypeAddedToJsonSerializerContext.cs
@@ -0,0 +1,28 @@
+﻿using WasabiBot.Core.Interfaces;
+using WasabiBot.DataAccess;
+
+namespace WasabiBot.UnitTests.DataAccess.Messages;
+
+public class TypeAddedToJsonSerializerContext
+{
+    [Test]
+    public void MyTest()
+    {
+        var handlerTypes = FindTypesImplementingInterface(typeof(IMessageHandler<>));
+        foreach (var handlerType in handlerTypes)
+        {
+            var handlerJsonTypeInfo = DataAccessJsonContext.Default.GetTypeInfo(handlerType);
+            if (handlerJsonTypeInfo is null)
+            {
+                Assert.Fail($"Type {handlerType.Name} was not added to the JsonSerializerContext");
+            }
+        }
+    }
+    
+    private IEnumerable<Type> FindTypesImplementingInterface(Type interfaceType)
+    {
+        var assemblies = System.Reflection.Assembly.Load("WasabiBot.DataAccess");
+        
+        return assemblies.GetTypes().Where(t => interfaceType.IsAssignableFrom(t) && t is { IsClass: true, IsAbstract: false });
+    }
+}
