3a856439aafb0911e7a6ccbc2b716c31b5893663
Kyle Hickman <kyle@txhickman.com>
2024-10-28 21:45:41 -0500

    upgrade packages

--- src/WasabiBot.DataAccess/Settings/EnvironmentVariables.cs
+++ src/WasabiBot.DataAccess/Settings/EnvironmentVariables.cs
@@ -3,7 +3,6 @@ namespace WasabiBot.DataAccess.Settings;
 
 public class EnvironmentVariables
 {
-    public string ENVIRONMENT { get; set; } = null!;
     public string DISCORD_PUBLIC_KEY { get; set; } = null!;
     public string DISCORD_TOKEN { get; set; } = null!;
     public string DISCORD_APPLICATION_ID { get; set; } = null!;
