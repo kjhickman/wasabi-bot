d5275b68c4ad79de4aa810dde2d9eedabdc7765f
Kyle Hickman <kyle@txhickman.com>
2024-11-03 16:58:01 -0600

    remove dotenv

--- tools/WasabiBot.MigrationsRunner/Program.cs
+++ tools/WasabiBot.MigrationsRunner/Program.cs
@@ -1,12 +1,7 @@
-﻿using dotenv.net;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using WasabiBot.MigrationsRunner;
 
-DotEnv.Load();
-var configuration = new ConfigurationBuilder()
-    .AddEnvironmentVariables()
-    .Build();
-
+var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
 var connectionString = configuration.GetConnectionString("wasabiBotDb");
 
 if (string.IsNullOrWhiteSpace(connectionString))
