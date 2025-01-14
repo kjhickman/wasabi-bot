6e12d79cb48ef0841b9f50119d9e600740c653f2
Kyle Hickman <kyle@txhickman.com>
2025-01-14 16:28:48 -0600

    refactor migrations runner env vars

--- tools/WasabiBot.MigrationsRunner/Program.cs
+++ tools/WasabiBot.MigrationsRunner/Program.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
-using WasabiBot.MigrationsRunner;
+﻿using WasabiBot.MigrationsRunner;
 
-var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
-var connectionString = configuration.GetConnectionString("Postgres");
+var connectionString = Environment.GetEnvironmentVariable("PG_CONNECTION_STRING");
 
 if (string.IsNullOrWhiteSpace(connectionString))
 {
-    throw new Exception($"Connection string is missing or invalid.");
+    throw new Exception("Connection string is missing or invalid.");
 }
 
 DatabaseUtility.Initialize(connectionString);
