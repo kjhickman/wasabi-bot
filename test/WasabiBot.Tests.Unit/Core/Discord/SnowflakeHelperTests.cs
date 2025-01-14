4919ca3f4d9144f2efc3195664a172f8b9efd4b5
Kyle Hickman <kyle@txhickman.com>
2025-01-13 19:25:39 -0600

    switch to xunit

--- test/WasabiBot.Tests.Unit/Core/Discord/SnowflakeHelperTests.cs
+++ test/WasabiBot.Tests.Unit/Core/Discord/SnowflakeHelperTests.cs
@@ -1,17 +1,18 @@
+using FluentAssertions;
 using WasabiBot.Discord;
 
-namespace WasabiBot.UnitTests.Core.Discord;
+namespace WasabiBot.Tests.Unit.Core.Discord;
 
 public class SnowflakeHelperTests
 {
-    [Test]
-    public async Task ConvertToDateTimeOffset_ValidSnowflake_ReturnsDateTimeOffset()
+    [Fact]
+    public void ConvertToDateTimeOffset_ValidSnowflake_ReturnsDateTimeOffset()
     {
         const long snowflakeId = 1293779028234211358;
         var expectedDateTimeOffset = DateTimeOffset.Parse("2024-10-10 03:35:57.583Z");
         
         var result = SnowflakeHelper.ConvertToDateTimeOffset(snowflakeId);
-        
-        await Assert.That(result).IsEqualTo(expectedDateTimeOffset);
+
+        result.Should().Be(expectedDateTimeOffset);
     }
 }
