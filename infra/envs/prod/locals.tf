8c8d3edf7cdadca1124faeff106b7f1836cca275
Kyle Hickman <kyle@txhickman.com>
2025-10-17 16:10:20 -0500

    chore: tf fmt

--- infra/envs/prod/locals.tf
+++ infra/envs/prod/locals.tf
@@ -9,19 +9,19 @@ data "terraform_remote_state" "network" {
 }
 
 locals {
-  environment                            = "prod"
-  project                                = "wasabi-bot"
-  api_service_name                       = "wasabi-bot-api"
-  api_container_name                     = "${local.api_service_name}-${local.environment}"
-  api_log_group_name                     = "/aws/ecs/${local.api_container_name}"
-  api_task_execution_role_arn            = data.terraform_remote_state.network.outputs.ecs_task_execution_role_arn
-  api_task_role_arn                      = data.terraform_remote_state.network.outputs.ecs_task_role_arn
-  ecs_cluster_name                       = data.terraform_remote_state.network.outputs.ecs_cluster_name
-  vpc_id                                 = data.terraform_remote_state.network.outputs.vpc_id
-  public_subnet_ids                      = data.terraform_remote_state.network.outputs.public_subnet_ids
-  vpc_link_id                            = data.terraform_remote_state.network.outputs.vpc_link_id
-  vpc_link_security_group_id             = data.terraform_remote_state.network.outputs.vpc_link_security_group_id
-  service_discovery_namespace_id         = data.terraform_remote_state.network.outputs.service_discovery_namespace_id
-  neon_project_id                        = data.terraform_remote_state.network.outputs.neon_project_id
-  neon_project_default_branch_id         = data.terraform_remote_state.network.outputs.neon_project_default_branch_id
+  environment                    = "prod"
+  project                        = "wasabi-bot"
+  api_service_name               = "wasabi-bot-api"
+  api_container_name             = "${local.api_service_name}-${local.environment}"
+  api_log_group_name             = "/aws/ecs/${local.api_container_name}"
+  api_task_execution_role_arn    = data.terraform_remote_state.network.outputs.ecs_task_execution_role_arn
+  api_task_role_arn              = data.terraform_remote_state.network.outputs.ecs_task_role_arn
+  ecs_cluster_name               = data.terraform_remote_state.network.outputs.ecs_cluster_name
+  vpc_id                         = data.terraform_remote_state.network.outputs.vpc_id
+  public_subnet_ids              = data.terraform_remote_state.network.outputs.public_subnet_ids
+  vpc_link_id                    = data.terraform_remote_state.network.outputs.vpc_link_id
+  vpc_link_security_group_id     = data.terraform_remote_state.network.outputs.vpc_link_security_group_id
+  service_discovery_namespace_id = data.terraform_remote_state.network.outputs.service_discovery_namespace_id
+  neon_project_id                = data.terraform_remote_state.network.outputs.neon_project_id
+  neon_project_default_branch_id = data.terraform_remote_state.network.outputs.neon_project_default_branch_id
 }
