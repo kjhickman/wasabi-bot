b290ca2fb2a3c36af957474c036bf6957ff577bf
Kyle Hickman <kyle@txhickman.com>
2025-11-11 22:54:13 -0600

    move token generator into own component

--- src/WasabiBot.Api/Components/Pages/Home.razor.cs
+++ src/WasabiBot.Api/Components/Pages/Home.razor.cs
@@ -1,7 +1,6 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using WasabiBot.Api.Infrastructure.Auth;
 
 namespace WasabiBot.Api.Components.Pages;
 
@@ -10,19 +9,10 @@ public partial class Home : ComponentBase
     [CascadingParameter]
     private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
 
-    [Inject]
-    private ApiTokenFactory TokenFactory { get; set; } = null!;
-
-    [SupplyParameterFromForm]
-    private bool ShouldGenerateToken { get; set; }
-
     private bool IsAuthenticated { get; set; }
     private string? Username { get; set; }
     private string? GlobalName { get; set; }
     private string? DisplayName { get; set; }
-    private string? GeneratedToken { get; set; }
-    private DateTimeOffset? TokenExpiresAt { get; set; }
-    private string? ErrorMessage { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
@@ -41,45 +31,6 @@ public partial class Home : ComponentBase
                     ?? user.FindFirst("urn:discord:user:globalname")?.Value;
 
                 DisplayName = GlobalName ?? Username ?? "User";
-                
-                // Generate token if form was submitted
-                if (ShouldGenerateToken)
-                {
-                    if (TokenFactory.TryCreateToken(user, out var token))
-                    {
-                        GeneratedToken = token;
-                        TokenExpiresAt = DateTimeOffset.UtcNow.Add(TokenFactory.Lifetime);
-                        ErrorMessage = null;
-                    }
-                    else
-                    {
-                        ErrorMessage = "Unable to generate token. Missing user information.";
-                        GeneratedToken = null;
-                        TokenExpiresAt = null;
-                    }
-                }
-            }
-        }
-    }
-
-    private async Task HandleSubmit()
-    {
-        if (AuthenticationStateTask is not null)
-        {
-            var authState = await AuthenticationStateTask;
-            var user = authState.User;
-
-            if (TokenFactory.TryCreateToken(user, out var token))
-            {
-                GeneratedToken = token;
-                TokenExpiresAt = DateTimeOffset.UtcNow.Add(TokenFactory.Lifetime);
-                ErrorMessage = null;
-            }
-            else
-            {
-                ErrorMessage = "Unable to generate token. Missing user information.";
-                GeneratedToken = null;
-                TokenExpiresAt = null;
             }
         }
     }
---
+++ src/WasabiBot.Api/Components/Pages/TokenGenerator.razor.cs
@@ -0,0 +1,48 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using WasabiBot.Api.Infrastructure.Auth;
+
+namespace WasabiBot.Api.Components.Pages;
+
+public partial class TokenGenerator : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public required string DisplayName { get; set; }
+
+    [CascadingParameter]
+    private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+
+    [Inject]
+    private ApiTokenFactory TokenFactory { get; set; } = null!;
+
+    [SupplyParameterFromForm]
+    private bool ShouldGenerateToken { get; set; }
+
+    private string? GeneratedToken { get; set; }
+    private DateTimeOffset? TokenExpiresAt { get; set; }
+    private string? ErrorMessage { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        // Generate token if form was submitted
+        if (ShouldGenerateToken && AuthenticationStateTask is not null)
+        {
+            var authState = await AuthenticationStateTask;
+            var user = authState.User;
+
+            if (TokenFactory.TryCreateToken(user, out var token))
+            {
+                GeneratedToken = token;
+                TokenExpiresAt = DateTimeOffset.UtcNow.Add(TokenFactory.Lifetime);
+                ErrorMessage = null;
+            }
+            else
+            {
+                ErrorMessage = "Unable to generate token. Missing user information.";
+                GeneratedToken = null;
+                TokenExpiresAt = null;
+            }
+        }
+    }
+}
