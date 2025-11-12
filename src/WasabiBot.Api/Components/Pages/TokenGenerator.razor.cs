b249e6ca01b909b492cd9ce1dfc43323e4bd8565
Kyle Hickman <kyle@txhickman.com>
2025-11-12 11:31:12 -0600

    clean up token generator

--- src/WasabiBot.Api/Components/Pages/TokenGenerator.razor.cs
+++ src/WasabiBot.Api/Components/Pages/TokenGenerator.razor.cs
@@ -9,33 +9,27 @@ public partial class TokenGenerator(ApiTokenFactory tokenFactory) : ComponentBas
     [CascadingParameter]
     public required Task<AuthenticationState> AuthenticationStateTask { get; set; }
 
-    [SupplyParameterFromForm]
-    private bool ShouldGenerateToken { get; set; }
-
     private string? GeneratedToken { get; set; }
     private DateTimeOffset? TokenExpiresAt { get; set; }
     private string? ErrorMessage { get; set; }
 
-    protected override async Task OnInitializedAsync()
+    // Registered as the handler for tokenForm
+    protected async Task GenerateToken()
     {
-        // Generate token if form was submitted - results in entire page re-render. Temporary solution
-        if (ShouldGenerateToken)
-        {
-            var authState = await AuthenticationStateTask;
-            var user = authState.User;
+        var authState = await AuthenticationStateTask;
+        var user = authState.User;
 
-            if (tokenFactory.TryCreateToken(user, out var token))
-            {
-                GeneratedToken = token;
-                TokenExpiresAt = DateTimeOffset.UtcNow.Add(tokenFactory.Lifetime);
-                ErrorMessage = null;
-            }
-            else
-            {
-                ErrorMessage = "Unable to generate token. Missing user information.";
-                GeneratedToken = null;
-                TokenExpiresAt = null;
-            }
+        if (tokenFactory.TryCreateToken(user, out var token))
+        {
+            GeneratedToken = token;
+            TokenExpiresAt = DateTimeOffset.UtcNow.Add(tokenFactory.Lifetime);
+            ErrorMessage = null;
+        }
+        else
+        {
+            ErrorMessage = "Unable to generate token. Missing user information.";
+            GeneratedToken = null;
+            TokenExpiresAt = null;
         }
     }
 }
