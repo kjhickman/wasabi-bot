76b462f03c90766d1db102a740a2a7230a5f4eaf
Kyle Hickman <kyle@txhickman.com>
2026-03-28 15:31:34 +0000

    chore: more clean up

--- src/WasabiBot.Api/Infrastructure/Database/DependencyInjection.cs
+++ src/WasabiBot.Api/Infrastructure/Database/DependencyInjection.cs
@@ -8,11 +8,6 @@ public static class DependencyInjection
     public static void AddDbContext(this IHostApplicationBuilder builder)
     {
         builder.Services.AddDbContext<WasabiBotContext>(options =>
-        {
-            var connectionString = builder.Configuration.GetConnectionString("wasabi-db")
-                                   ?? builder.Configuration.GetConnectionString("wasabi_db");
-
-            options.UseNpgsql(connectionString);
-        });
+            options.UseNpgsql(builder.Configuration.GetConnectionString("wasabi_db")));
     }
 }
