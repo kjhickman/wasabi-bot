f5f06332524e6989ad00af6182ed0ecb829386f7
Kyle Hickman <kyle@txhickman.com>
2026-04-01 00:28:30 +0000

    feat: make token a POST; address other minor refactorings

--- src/WasabiBot.Api/Features/Token/GetToken.cs
+++ src/WasabiBot.Api/Features/Token/GetToken.cs
@@ -5,6 +5,12 @@ namespace WasabiBot.Api.Features.Token;
 
 public static class GetToken
 {
+    public static IResult Handle(HttpContext httpContext, ClaimsPrincipal user, ApiTokenFactory tokenFactory)
+    {
+        ApplyNoStore(httpContext);
+        return Handle(user, tokenFactory);
+    }
+
     public static IResult Handle(ClaimsPrincipal user, ApiTokenFactory tokenFactory)
     {
         if (!tokenFactory.TryCreateToken(user, out var token))
@@ -19,5 +25,12 @@ public static class GetToken
         };
 
         return Results.Ok(response);
+    }
+
+    private static void ApplyNoStore(HttpContext httpContext)
+    {
+        httpContext.Response.Headers.CacheControl = "no-store, no-cache, max-age=0";
+        httpContext.Response.Headers.Pragma = "no-cache";
+        httpContext.Response.Headers.Expires = "0";
     }
 }
---
+++ test/WasabiBot.UnitTests/Core/Extensions/ClaimsExtensionsTests.cs
@@ -0,0 +1,54 @@
+using System.Security.Claims;
+using WasabiBot.Api.Core.Extensions;
+using WasabiBot.UnitTests.Builders;
+
+namespace WasabiBot.UnitTests.Core.Extensions;
+
+public class ClaimsExtensionsTests
+{
+    [Test]
+    public async Task DisplayName_WithWhitespaceGlobalName_FallsBackToUsername()
+    {
+        var user = ClaimsPrincipalBuilder.Create()
+            .WithDiscordGlobalName("   ")
+            .WithDiscordUsername("testuser")
+            .Build();
+
+        await Assert.That(user.DisplayName).IsEqualTo("testuser");
+    }
+
+    [Test]
+    public async Task DisplayName_WithWhitespaceClaims_ReturnsDefaultUser()
+    {
+        var user = ClaimsPrincipalBuilder.Create()
+            .WithDiscordGlobalName(" ")
+            .WithDiscordGlobalNameAlt(" ")
+            .WithDiscordUsername(" ")
+            .WithName(" ")
+            .Build();
+
+        await Assert.That(user.DisplayName).IsEqualTo("User");
+    }
+
+    [Test]
+    public async Task DiscordAvatarUrl_WithoutAvatarHash_UsesDefaultDiscordAvatar()
+    {
+        var user = ClaimsPrincipalBuilder.Create()
+            .WithUserId("123456789")
+            .WithDiscordDiscriminator("1234")
+            .Build();
+
+        await Assert.That(user.DiscordAvatarUrl).IsEqualTo("https://cdn.discordapp.com/embed/avatars/4.png");
+    }
+
+    [Test]
+    public async Task DiscordAvatarUrl_WithWhitespaceUserId_ReturnsNull()
+    {
+        var user = ClaimsPrincipalBuilder.Create()
+            .WithUserId("   ")
+            .WithDiscordAvatar("avatarhash")
+            .Build();
+
+        await Assert.That(user.DiscordAvatarUrl).IsNull();
+    }
+}
