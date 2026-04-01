613b6014cf7c7aa835914c048df2f47fda36a337
Kyle Hickman <kyle@txhickman.com>
2026-04-01 03:20:20 +0000

    test: add more bunit tests

---
+++ test/WasabiBot.UnitTests/Components/NotFoundComponentTests.cs
@@ -0,0 +1,24 @@
+using Bunit;
+using WasabiBot.Api.Components.Pages;
+
+namespace WasabiBot.UnitTests.Components;
+
+public class NotFoundComponentTests : IDisposable
+{
+    private readonly BunitContext _context = new();
+
+    [Test]
+    public async Task Render_Shows404HeadingAndHomeLink()
+    {
+        var cut = _context.Render<NotFound>();
+
+        await Assert.That(cut.Markup).Contains("404");
+        await Assert.That(cut.Find("#not-found-heading").TextContent.Trim()).IsEqualTo("This page does not exist.");
+        await Assert.That(cut.Find("a[role='button']").GetAttribute("href")).IsEqualTo("/");
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
