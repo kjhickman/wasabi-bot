968c41f5952aabc5366ac858ae1f2169924075f5
Kyle Hickman <kyle@txhickman.com>
2026-04-01 21:08:01 -0500

    refactor: rearrange endpoints

--- src/WasabiBot.Api/Features/Credentials/CreateCredentialRequest.cs
+++
@@ -1,6 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace WasabiBot.Api.Features.Credentials;
-
-public sealed record CreateCredentialRequest(
-    [property: JsonPropertyName("name")] string Name);
